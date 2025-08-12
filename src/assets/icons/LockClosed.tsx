8b239f744c79a05a0ab51bf71bdf974ecf53d458
Bruno Ferraz Silveira <bfssfb@outlook.com.br>
2025-08-12 14:02:51 -0300

    chore: add icon asset

---
+++ src/assets/icons/LockClosed.tsx
@@ -0,0 +1,25 @@
+type IconProps = {
+  color: string
+  size: number
+}
+
+export function LockClosed({ color, size }: IconProps) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 24 24'
+      strokeWidth={1.5}
+      stroke={color}
+      width={size}
+      height={size}
+      className='size-6'
+    >
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z'
+      />
+    </svg>
+  )
+}
