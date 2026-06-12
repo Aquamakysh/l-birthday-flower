2d1417bab6335b1ff8e2567d750e4a75ea4aeae3
Bruno Ferraz Silveira <bfssfb@outlook.com.br>
2026-06-12 12:21:08 -0300

    fix: fix build configs

--- src/app/serwist/[path]/route.ts
+++ src/app/serwist/[path]/route.ts
@@ -1,11 +1,18 @@
 import { createSerwistRoute } from '@serwist/turbopack'
 
-export const { dynamic, dynamicParams, revalidate, generateStaticParams, GET } =
-  createSerwistRoute({
-    swSrc: 'src/app/sw.ts',
-    injectionPoint: 'self.__SW_MANIFEST',
-    globDirectory: '.',
-    globPatterns: [
-      'public/pwa/**/*'
-    ]
-  })
+export const {
+  dynamic,
+  dynamicParams,
+  revalidate,
+  generateStaticParams,
+  GET
+} = createSerwistRoute({
+  swSrc: 'src/app/sw.ts',
+  injectionPoint: 'self.__SW_MANIFEST',
+  globDirectory: '.',
+  globPatterns: ['public/pwa/**/*'],
+  useNativeEsbuild: true,
+  esbuildOptions: {
+    target: 'esnext'
+  }
+})
